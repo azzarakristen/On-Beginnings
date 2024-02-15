7c0b02815fab80708f7574e0dd78132881a2457f
azzarakristen <145416759+azzarakristen@users.noreply.github.com>
2024-02-15 11:31:54 -0500

    Add files via upload

---
+++ Project_1/script.js
@@ -0,0 +1,30 @@
+window.addEventListener("load", function () {
+  init()
+});
+
+
+const top_id = "paragraph";
+
+
+const init = ()=>{
+ let top_div = document.getElementById(top_id);
+ 
+ top_div.innerHTML = section1;
+
+
+
+};
+
+const section1 = `In life, the number of beginnings is exactly equal to the number of endings: no one has yet to begin a life who will not end it. <br><br>
+
+In poetry, the number of beginnings so far exceeds the number of endings that we cannot even conceive of it. Not every poem is finished—one poem is abandoned, another catches fire and is carried away by the wind, which may be an ending, but it is the ending of a poem without an end. <br><br>
+
+Paul Valéry, the French poet and thinker, once said that no poem is ever ended, that every poem is merely abandoned. This saying is also attributed to Stéphane Mallarmé, for where quotations begin is in a cloud. <br><br>
+
+Paul Valéry also described his perception of first lines so vividly, and to my mind so accurately, that i have never forgotten it: the opening line of a poem, he said, is like finding a fruit on the ground, a piece of fallen fruit you have never seen before, and the poet’s task is to create the tre from which such a fruit would fall. 
+
+In the beginning was the Word. Western civilization rests upon those words. And yet there is a lively group of thinkers who believe that in the beginning was the Act. that nothing can precede action—no breath before act, no thought before act, no pervasive love before some kind of act. <br><br>
+
+`
+const section2 = ``
+
